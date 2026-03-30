5902c2f76115a063924989c80f0bbc47e949f72e
eldaahax <146485957+eldaahax@users.noreply.github.com>
2026-03-30 18:42:02 +0200

    Login page into a react Component /Validation

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,6 @@
-import About from "../../LABORATORI-1/src/pages/AboutUs"
 export default function App(){
     return(
         <>
-     <About/>
         </>
     )
 }
---
+++ src/pages/Login.jsx
@@ -0,0 +1,105 @@
+import { useState } from "react";
+
+export default function Login() {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const [userError, setUserError] = useState("");
+  const [passError, setPassError] = useState("");
+
+  const userregex = /^[A-Za-z0-9.-_!]+@[a-zA-Z-_]+\.[a-z]{3}$/;
+  const passRegex =
+    /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*])[A-Za-z\d!@#$%^&*]{8,20}$/;
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    let hasError = false;
+
+    setUserError("");
+    setPassError("");
+
+    if (username.trim() === "") {
+      setUserError("Username is required");
+      hasError = true;
+    } else if (!userregex.test(username)) {
+      setUserError("Username should be a valid email");
+      hasError = true;
+    }
+
+    if (password.trim() === "") {
+      setPassError("Password is required");
+      hasError = true;
+    } else if (!passRegex.test(password)) {
+      setPassError(
+        "Password must have uppercase, lowercase, number, and symbol",
+      );
+      hasError = true;
+    }
+  };
+
+  return (
+    <div className="flex justify-center items-center w-full h-screen relative <font-serif">
+      <div class=" w-full max-w-md p-8 rounded-xl    ">
+        <h1 class="text-[36px] font-bold text-[#0F766E] text-center tracking-widest mb-6">
+          LOG-IN
+        </h1>
+
+        <form onSubmit={handleSubmit} class="space-y-4" id="form">
+          <div class="flex flex-col">
+            <label for="username" class="mb-1 font-semibold text-[#134E4A]">
+              Username
+            </label>
+            <input
+              type="text"
+              value={username}
+              onChange={(e) => {
+                setUsername(e.target.value);
+                setUserError("");
+              }}
+              placeholder="Email"
+              aria-live="polite"
+              className={`border p-2 w-full rounded-[14px] ${
+                userError ? "border-red-500 " : "border-gray-300"
+              }`}
+            />
+            <p className="text-red-500 text-sm ">{userError}</p>
+          </div>
+
+          <div class="flex flex-col">
+            <label for="password" class="mb-1 font-semibold text-[#134E4A]">
+              Password
+            </label>
+            <input
+              type="password"
+              value={password}
+              onChange={(e) => {
+                setPassword(e.target.value);
+                setPassError("");
+              }}
+              placeholder="Password"
+              aria-live="polite"
+              className={`border p-2 w-full rounded-[14px] ${
+                passError ? "border-red-500 " : "border-gray-300"
+              }`}
+            />
+            <p className="text-red-500 text-sm">{passError}</p>
+          </div>
+
+          <input
+            type="submit"
+            value="Login"
+            class="w-full bg-[#0F766E] text-white font-bold py-2 rounded-lg cursor-pointer  "
+          />
+
+          <p class="text-black text-center font-semibold">
+            Don't have an account?
+            <a href="" class="text-[#0F766E] font-bold hover:underline">
+              Sign Up
+            </a>
+          </p>
+        </form>
+      </div>
+    </div>
+  );
+}
