232fa9fdd62e486e80c482612fd5cf41607bda4f
eldaahax <146485957+eldaahax@users.noreply.github.com>
2026-04-21 11:05:30 +0200

    Package's/serve.js/database conn

---
+++ Backend/config/database.js
@@ -0,0 +1,12 @@
+const { Sequelize } = require("sequelize");
+
+const sequelize = new Sequelize(
+  "LAB", 
+  "root", 
+  "", 
+  {
+  host: "127.0.0.1",
+  dialect: "mysql",
+});
+
+module.exports = sequelize;
