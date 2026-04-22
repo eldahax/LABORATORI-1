1ab7d407340b738fe4e2139d86f00c72db6ef79f
eldaahax <146485957+eldaahax@users.noreply.github.com>
2026-04-22 14:13:19 +0200

    migrations

---
+++ Backend/Migrations/20260421100738-create-appointments.js
@@ -0,0 +1,71 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('appointments', {
+      appointment_id: {
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+        allowNull: false
+      },
+      patient_id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        references: {
+          model: 'patients',
+          key: 'patient_id'
+        },
+        onUpdate: 'CASCADE',
+        onDelete: 'CASCADE'
+      },
+      doctor_id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        references: {
+          model: 'doctors',
+          key: 'doctor_id'
+        },
+        onUpdate: 'CASCADE',
+        onDelete: 'CASCADE'
+      },
+      room_id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        references: {
+          model: 'rooms',
+          key: 'room_id'
+        },
+        onUpdate: 'CASCADE',
+        onDelete: 'CASCADE'
+      },
+      created_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP')
+      },
+      appointment_date_time: {
+        type: Sequelize.DATE,
+        allowNull: false
+      },
+      duration: {
+        type: Sequelize.INTEGER,
+        allowNull: false
+      },
+      appointment_status: {
+        type: Sequelize.ENUM('confirmed', 'pending', 'cancelled', 'complete'),
+        defaultValue: 'pending',
+        allowNull: false
+      },
+      description: {
+        type: Sequelize.STRING(100),
+        allowNull: false
+      }
+    });
+  },
+
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('appointments');
+  }
+};
